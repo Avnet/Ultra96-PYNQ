30dc5cd8f0b3de62e0ba1b61bc6b724902b60719
Rock Qu <yunq@xilinx.com>
2018-07-09 11:52:29 -0700

    add mraa package

---
+++ Ultra96/packages/mraa/pre.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+target=$1
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo mkdir $target/root/mraa_build
+sudo cp $script_dir/ultra96.patch $target/root/mraa_build
