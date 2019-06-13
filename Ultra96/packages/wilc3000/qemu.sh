1ce7412052af2e2a8a5dffc451035b8025bb78ee
Rock Qu <yunq@xilinx.com>
2019-06-13 15:08:09 -0700

    fix wpa_ap service

--- Ultra96/packages/wilc3000/qemu.sh
+++ Ultra96/packages/wilc3000/qemu.sh
@@ -1,29 +1,49 @@
-#! /bin/bash
+#!/bin/bash
 
-set -x
 set -e
-
-cd /wilc_bld
+set -x
 
 # Install firmware files on root filesystem
+cd /wilc_bld
 git clone https://github.com/linux4wilc/firmware
 cd firmware
 git checkout tags/wilc_linux_15_2
 cp /wilc_bld/firmware/wilc3000_wifi_firmware.bin /lib/firmware/mchp/
 cp /wilc_bld/firmware/wilc3000_ble_firmware.bin /lib/firmware/mchp/
-
 cd /
 rm -rf wilc_bld
-
-# Register wifi driver (depmod doesn't work here)
-# depmod -a
 
 # Start module on bootup
 echo "wilc-sdio" >> /etc/modules
 
 # Wilc cannot do wlan1 only wlan0 and p2p0, so fix that for host ap
-echo "INTERFACES=\"usb0 p2p0\"" > /etc/default/isc-dhcp-server
-cp /usr/local/share/wpa_ap/ap.sh /usr/local/share/wpa_ap/ap_v1.sh
-cp /usr/local/share/wpa_ap/ap_v2.sh /usr/local/share/wpa_ap/ap.sh
 chmod a+x /usr/local/share/wpa_ap/ap.sh
+if [ -f /etc/default/isc-dhcp-server ] && \
+	grep -q "INTERFACES=" /etc/default/isc-dhcp-server; then
+	if ! grep -q "p2p0" /etc/default/isc-dhcp-server; then
+		sed -i 's/\(INTERFACES=\)"\(.*\)"/\1"\2 p2p0"/g' \
+		/etc/default/isc-dhcp-server
+	fi
+else
+	echo 'INTERFACES="p2p0"' > /etc/default/isc-dhcp-server
+fi
 
+cat - >> /etc/dhcp/dhcpd.conf <<EOT
+
+subnet 192.168.2.0 netmask 255.255.255.0 {
+   option subnet-mask 255.255.255.0;
+   option broadcast-address 192.168.2.0;
+   range 192.168.2.2 192.168.2.100;
+}
+EOT
+
+cat - > /etc/network/interfaces.d/wlan0 <<EOT
+iface wlan0 inet dhcp
+	wireless_mode managed
+	wireless_essid any
+	wpa-driver wext
+	wpa-conf /etc/wpa_supplicant.conf
+EOT
+
+# Uncomment the following line to enable wireless access point by default
+# systemctl enable wpa_ap.service
--- Ultra96/packages/wilc3000/wpa_ap/ap.sh
+++ Ultra96/packages/wilc3000/wpa_ap/ap.sh
@@ -15,7 +15,7 @@ done
 cd /usr/local/share/wpa_ap
 
 # Create a new managed mode interface wlan1 to run AP
-iw phy phy0 interface add wlan1 type managed
+iw phy phy0 interface add p2p0 type managed
 
 hid=$(ifconfig p2p0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed "s/:/_/g")
 ip=192.168.2.1
@@ -24,7 +24,7 @@ sed "s/pynq/pynq_$hid/g" wpa_ap.conf > wpa_ap_actual.conf
 
 ifconfig p2p0 down
 sleep 2
-wpa_supplicant -c ./wpa_ap_actual.conf  --ip2p0 &
+wpa_supplicant -c ./wpa_ap_actual.conf  -ip2p0 &
 
 ifconfig p2p0 $ip
 
